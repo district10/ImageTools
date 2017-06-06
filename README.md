4a08a6ac297818df92abf68a1ed75587b9d5e127
TANG ZhiXiong <dvorak4tzx@qq.com>
2017-06-06 09:41:22 +0800

    need more time to make it work

--- README.md
+++ README.md
@@ -1,74 +1,9 @@
-![](images/02_Matches.png)
+# ImageTools
 
-~~Mod based on <https://github.com/colmap/colmap>.~~
+Some code (vfeat, image reading/writing) extracted from <https://github.com/colmap/colmap>.
 
-后来还是用了 OpenMVG。本目录下有 Windows 二进制：PanoMatch.exe。
-
-比如有两张全景图：
-
-site1.jpg:
-
-![](images/site1.jpg)
-
-site2.jpg:
-
-![](images/site2.jpg)
-
-处理：
-
-```bash
-$ ./PanoMatch.exe -t SIFT -a images/site1.jpg -b images/site2.jpg
-0       4
-1       4
-2       5
-3       3
-4       3
-5       0
-6       3
-7       7
-8       5
-9       5
-10      0
-11      0
-12      0
-13      0
-14      0
-15      0
-16      0
-17      0
-18      0
-19      0
-20      0
-21      0
-22      0
-23      0
-24      0
-25      0
-26      0
-27      0
-28      0
-29      0
-30      0
-31      0
-32      0
-33      0
-34      0
-35      0
-223 #Features on image A
-204 #Features on image B
-39 #matches with Distance Ratio filter
-```
-
-结果：
-
-00_images.jpg：
-
-![](00_images.jpg)
-
-01_features.jpg：
-
-![](01_features.jpg)
-
-02_Matches.svg：
+-   ShiftImage：图像移位
+-   SiftMatch：特征匹配
+-   PanoMatch：特征匹配（2），针对全景图
 
 ![](02_Matches.png)
--- examples/SiftMatch.cpp
+++ examples/SiftMatch.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <stdio.h>
 
 #include "Configs.h"
@@ -13,28 +14,29 @@ using namespace std;
 
 const static BitmapColor<uint8_t> red(255, 0, 0);
 const static BitmapColor<uint8_t> green(0, 255, 0);
+const static BitmapColor<uint8_t> white(255, 255, 255);
 const static BitmapColor<uint8_t> blue(0, 0, 255);
 
 int main ( int argc, char **argv )
 {
+    if (argc > 1 && (argv[1] == "-h" || argv[1] == "--help")) {
+        cout << "Usage:\n\t" << argv[0] << "<image1> <image2> [<output>]\n";
+    }
+
     Bitmap img1, img2;
     string imgurl1 = CMAKE_SOURCE_DIR "/images/site1.jpg";
     string imgurl2 = CMAKE_SOURCE_DIR "/images/site2.jpg";
-    if (argc >= 3) {
-        imgurl1 = argv[1];
-        imgurl2 = argv[2];
-    }
-    if (
-        !img1.Read(imgurl1, true) || !img2.Read(imgurl2, true) ||
-        img1.Width() != img2.Width() || img1.Height() != img2.Height()
-    ) {
-        cout << "read image error\n";
-        cout << "Usage:\n"
-            << "\t" << argv[0] << "\n"
-            << "\t" << argv[0] << "image1 image2\n";
+    string imgurl3 = "matched.jpg";
+    if (argc > 1) { imgurl1 = argv[1]; }
+    if (argc > 2) { imgurl2 = argv[2]; }
+    if ( !img1.Read(imgurl1, true) ) {
+        cout << "Error reading image '" << imgurl1 << "'\n";
         return 1;
     }
-    cout << "input: " << imgurl1 << ", " << imgurl2 << "\n";
+    if ( !img2.Read(imgurl2, true) ) {
+        cout << "Error reading image '" << imgurl2 << "'\n";
+        return 1;
+    }
 
     FeatureKeypoints keypoints1, keypoints2;
     FeatureDescriptors descriptors1, descriptors2;
@@ -43,15 +45,18 @@ int main ( int argc, char **argv )
         !ExtractSiftFeaturesCPU(img1, keypoints1, descriptors1, sift_options) ||
         !ExtractSiftFeaturesCPU(img2, keypoints2, descriptors2, sift_options)
     ) {
-        cout << "feature extraction error\n";
+        cout << "Feature extraction error\n";
         return 2;
     }
-    cout << "#size: " << keypoints1.size() << ", " << keypoints2.size() << "\n";
-    img1.DrawPoints(keypoints1, red);
+    cout << "#FeaturePoints: " << keypoints1.size() << ", " << keypoints2.size() << "\n";
+    img1.DrawPoints(keypoints1, blue);
     img2.DrawPoints(keypoints2, green);
 
     Bitmap img12;
-    img12.Allocate(img1.Width(), img1.Height()*2, img1.IsRGB());
+    img12.Allocate(
+        max(img1.Width(),img2.Width()), 
+        img1.Height()+img2.Height(), img1.IsRGB()
+    );
     const int lineBytes = (img1.IsRGB() ? 3 : 1) * img1.Width();
     for (int i = 0; i < img1.Height(); ++i) {
         const uint8_t *line1 = img1.GetScanline(i);
@@ -60,6 +65,10 @@ int main ( int argc, char **argv )
         memcpy(img12.GetScanline(i+img1.Height()), line2, lineBytes);
     }
 
+    FILE *ofp = fopen("matches.txt", "w");
+    if (ofp) {
+        fprintf(ofp, "a: %s, b: %s\n", imgurl1, imgurl2);
+    }
     FeatureMatches matches;
     MatchSiftFeaturesCPU(SiftMatchOptions(), descriptors1, descriptors2, matches);
     for (int i = 0; i < matches.size(); ++i) {
@@ -69,15 +78,24 @@ int main ( int argc, char **argv )
             int x0 = keypoints1[idx1].x;
             int y0 = keypoints1[idx1].y;
             int x1 = keypoints2[idx2].x;
-            int y1 = keypoints2[idx2].y + img1.Height();
-            // img12.DrawLine(x0, y0, x1, y1, blue);
-            // img12.DrawLine(x0, y0, x1, y1, blue);
-            img12.DrawPoint(x0, y0, red);
-            img12.DrawPoint(x1, y1, green);
+            int y1 = keypoints2[idx2].y;
+            if (ofp) {
+                fprintf(ofp, 
+                    "%lf, %lf <-> %lf, %lf\n",
+                    (double)x0/img1.Width(), (double)y0/img1.Height(),
+                    (double)x1/img2.Width(), (double)y1/img2.Height()
+                );
+            }
+            y1 += img1.Height();
+            img12.DrawLine(x0, y0, x1, y1, red);
+            img12.DrawLine(x0, y0, x1, y1, red);
+            img12.DrawPoint(x0, y0, white);
+            img12.DrawPoint(x1, y1, white);
         }
     }
+    fclose(ofp);
     cout << "#matches: " << matches.size() << "\n";
-    img12.Write("matched.jpg");
+    img12.Write(imgurl3);
 
     return 0;
 }
